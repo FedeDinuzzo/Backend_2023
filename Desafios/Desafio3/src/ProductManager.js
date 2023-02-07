84383751eb67026a638988f1331d070664cb0bd4
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2023-02-06 22:35:29 -0300

    Desafio 3

---
+++ Desafios/Desafio3/src/ProductManager.js
@@ -0,0 +1,134 @@
+import { promises as fs, existsSync, writeFileSync } from "fs"
+
+export default class ProductManager {
+  constructor(path) {
+    this.path = path
+  }
+
+  checkFile = () => {
+    // If the file doesn't exists, we create it. Otherwise, do nothing
+    !existsSync(this.path) && writeFileSync(this.path, "[]", "utf-8");
+  };
+
+  addProduct = async (newProduct) => {
+    this.checkFile()
+    try {
+      let contenido = await fs.readFile(this.path, "utf-8")
+      let aux = JSON.parse(contenido)
+      aux.push(newProduct)
+      await fs.writeFile(this.path, JSON.stringify(aux))
+      console.log("Product added succesfully")
+    } catch (error) {
+      error
+    }
+  }
+
+  getProducts = async () => {
+    this.checkFile()
+    try {
+      const read = await fs.readFile(this.path , "utf-8")
+      const data = JSON.parse(read)
+      if (data.lenght !== 0) {
+        console.log("This are all the products: ")
+        return console.log(data)
+      } else {
+        return console.log(`The path ${this.path} has no products`)
+      }
+    } catch (error) {
+      error
+    }
+  }
+
+  getProductsById = async (id) => {
+    this.checkFile()
+    try {
+      const read = await fs.readFile(this.path , "utf-8")
+      const data = JSON.parse(read)
+      const findProduct = data.find(prod => prod.id === id)
+      if (findProduct) {
+        console.log("The following product has been found: ")
+        return console.log(findProduct)
+      } else {
+        return console.log(`The product with the id: ${id} has not been found`)
+      }
+    } catch (error) {
+      error
+    }
+  }
+
+  updateProduct = async (id, entry, value) => {
+    this.checkFile()
+    try {
+      const read = await fs.readFile(this.path, "utf-8")
+      const data = JSON.parse(read)
+      const index = data.findIndex((product) => product.id === id)
+      if (!data[index][entry]) {
+        return console.log("Product not found")
+      } else {
+        data[index][entry] = value;
+        await fs.writeFile(this.path, JSON.stringify(data, null, 2));
+        console.log("Product has been updated to: ")
+        return console.log(data[index]);
+      }
+    } catch (error) {
+      error
+    }
+  }
+
+  deleteProduct = async (id) => {
+    this.checkFile()
+    try {
+      const read = await fs.readFile(this.path , "utf-8")
+      const data = JSON.parse(read)
+      const deletedProduct = JSON.stringify(data.find(prod => prod.id === id))
+      const newData = data.filter(prod => prod.id !== id)
+      await fs.writeFile(this.path, JSON.stringify(newData), "utf-8")
+      console.log("The following product has been deleted: ")
+      return console.log(deletedProduct)
+    } catch (error) {
+      error
+    }
+  }
+}  
+
+class Product {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title
+    this.description = description
+    this.price = price
+    this.thumbnail = thumbnail
+    this.code = code
+    this.stock = stock
+    this.id = Product.addId() 
+  }
+
+  static addId() {
+    if(this.idIncrement) { 
+      this.idIncrement++
+    } else {
+      this.idIncrement = 1
+    }
+    return this.idIncrement
+  }
+}
+
+// Test
+// const manager = new ProductManager("./products.json")
+
+// const product1 = new Product("Iphone", "Smartphone", 1200, "insertar thumbnail" , "SKU123", 1000)
+// const product2 = new Product("Samsung", "Smartphone", 1100, "insertar thumbnail" , "SKU124", 950)
+
+// const test = async () => {
+//   await manager.getProducts()
+//   await manager.addProduct(product1)
+//   await manager.addProduct(product2)
+//   await manager.getProducts()
+//   await manager.getProductsById(1)
+//   await manager.updateProduct(1, "title", "Xiaomi")
+//   await manager.updateProduct(1, "stock", 1200)
+//   await manager.getProducts()
+//   await manager.deleteProduct(1)
+//   await manager.getProducts()
+// }
+
+// test()
---
+++ Desafios/Desafio3/src/app.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { ProductManager } from './ProductManager'
+
+const app = express()
+const PORT = 8080
+const manager = new ProductManager("./src/products.json");
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send("Este es el desafio 3")
+})
+
+// Reads the products file and returns it in an object
+// If you pass a limit by query params it returns that and if not returns all the products
+app.get('/products', async (req, res) => {
+  try {
+    const products = await manager.getProducts()
+    const { limit } = req.query
+    if (limit) {
+      const productsLimit = products.slice(0, parseInt(limit))
+      res.send(`This are the products you ordered: ${JSON.stringify(productsLimit)}`)
+    } else {
+      res.send(`This are all the products: ${JSON.stringify(products)}`)
+    }
+  } catch {
+    console.log(`Something wen't wrong, cannot get products`)
+  }
+})
+
+// Looks for the product by its id and returns it
+app.get('/products/:pid', async (req, res) => {
+  try {
+    const product = await manager.getProductsById(parseInt(req.params.id))
+    res.send(`The product with the id: ${product.id} is: ${JSON.stringify(product)}`)
+  } 
+  catch {
+    res.send("The product doesn't exist")
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`)
+})
