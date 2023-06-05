60dd62145f0c4c35c892b7415831ad3a67afaea6
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2023-06-05 20:06:28 -0300

    Clase28

--- Clases/Clase28/Resumen.js
+++ Clases/Clase28/Resumen.js
@@ -1,2 +1,27 @@
 // Continuacion implementacion clase 27
 // Arquitectura del servidor: Diseño
+
+// Tener codigo reutilizable
+// Pensar en codigo escalable
+// Homologacion de desing patterns
+
+// Adoptar un modelo por capas
+// Usar carpeta services
+// Manejo de entornos (al menos 2 o 3)
+// Testing (Robot Framework)
+// Loggear y documentor
+
+// Patron de diseno - forma de crear software que ya esta creada
+
+// Patrones de express
+// Cadena de responsabilidades
+// Decorador - mantiene un objeto inicial genereico pero al ser utilizado se transforma (multer)
+// Proxy, divde la app (Routting pattern), el patron mas comun es Express
+// MVC
+// Patron Singleton, instancia global de la app (DAO)
+
+// Comunicacion Backend Frontend
+// Server Side Rendering
+
+// React con Next como ejemplo de front
+
