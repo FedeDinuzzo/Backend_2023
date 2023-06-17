c1eee24af7158a758c5552a04b157d43b33644bd
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2023-06-17 15:54:39 -0300

    Mitad clase 39

--- Clases/Clase39/Resumen.js
+++ Clases/Clase39/Resumen.js
@@ -1,1 +1,14 @@
-// Documentacion
+// Documentacion en Rest API
+
+// Importancia de la documentacion
+// Brindiar suficiente informacion para que el proyecto sea entendible
+// Los comentarios son el legado del codigo
+// A veces se documenta al final
+
+// Swagger 
+// Mantener cada modulo de la API dentro de un espectro de entendimiento solido
+// Open API specification
+
+// Archivo de configuracion 
+// Proyecto de prueba 
+// https://github.com/CoderContenidos/RecursosBackend-Adoptme
