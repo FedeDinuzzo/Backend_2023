4e29597a761645af0860283ecedd30d6726d09c5
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2023-06-19 00:15:12 -0300

    After clase42 Nest CRUD

---
+++ Clases/Clase42After/Resumen.js
@@ -0,0 +1,18 @@
+// NestJS CRUD
+// Clase42 -> Nest
+
+// Terminal
+// nest g resource Users
+// RestAPI
+
+// Genera modulo: src/users
+// http://localhost:3000/users
+// Postman crear user
+// Metodo POST:
+// {
+//   "id": 1,
+//   "first_name": "Fernando",
+//   "last_name": "Alonso",
+//   "password": "La33",
+//   "email": "fer@alo.com"
+// }
